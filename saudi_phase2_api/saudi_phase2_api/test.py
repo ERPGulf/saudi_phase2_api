ed3fc1230495c20555f5625a7ce62285a12b7378
HusnaMa <husna@htsqatar.com>
2023-09-18 08:16:54 +0000

    Testing post request

--- saudi_phase2_api/saudi_phase2_api/test.py
+++ saudi_phase2_api/saudi_phase2_api/test.py
@@ -1,2 +1,12 @@
-import json
-print("Hello world2222")
+import os
+from twilio.rest import Client
+account_sid = 'ACbfd204d4101882f6a20c4e06136d2bb3'
+auth_token = '358440022aff2ce4006f15ceeba2bf25'
+client = Client(account_sid, auth_token)
+message = client.messages.create(
+  from_='+18789999387',
+  body='hello testing ..',
+  to='+917306204060'
+)
+print(message.sid)
+
