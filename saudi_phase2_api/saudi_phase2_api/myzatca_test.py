7c3373bab2651b132a7b802f670daa52fc4ee72d
HusnaMa <husna@htsqatar.com>
2023-10-19 15:57:59 +0000

    zatca qr

--- saudi_phase2_api/saudi_phase2_api/myzatca_test.py
+++ saudi_phase2_api/saudi_phase2_api/myzatca_test.py
@@ -55,8 +55,9 @@ def send_invoice_for_clearance_normal():
         # Sending invoice for clearance through normal python library - Farook
         # sys.exit()
         invoiceHash = get_InvoiceHash()
-        
-        signedXmlFilePath = "/opt/bench3/frappe-bench/sites/signedXML_withQR.xml"
+        # print(invoiceHash)
+        signedXmlFilePath="/opt/oxy/frappe-bench/apps/saudi_phase2_api/saudi_phase2_api/saudi_phase2_api/test_xml_formatter.xml"
+        # signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXML_withQR.xml"
         print("signedXmlFilePath:   " + signedXmlFilePath)
         token,secret = create_security_token_from_csr()
         uuid =get_UUID()
@@ -66,7 +67,7 @@ def send_invoice_for_clearance_normal():
             # print(base64_encoded)
             base64_decoded = base64_encoded.decode("utf-8")
             # print(base64_decoded)
-            # print(xml)
+            print(xml)
         url = "https://gw-fatoora.zatca.gov.sa/e-invoicing/developer-portal/compliance/invoices"
 
         payload = json.dumps({
@@ -97,8 +98,8 @@ def send_invoice_for_clearance_normal():
 def get_signed_xml_invoice_for_clearance():
         #Creating and returning sbDigestValue, xmlSigned, signedXmlFilePath - Farook
         
-        # signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXml.xml"
-        signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXML_withQR.xml"
+        signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXml.xml"
+        # signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXML_withQR.xml"
         xmlSigned = chilkat2.Xml()
         success = xmlSigned.LoadXmlFile(signedXmlFilePath)
         if (success == False):
@@ -113,7 +114,6 @@ def get_signed_xml_invoice_for_clearance():
             sys.exit()
         # print("DigestValue = " + sbDigestValue.GetAsString())
         return sbDigestValue, xmlSigned, signedXmlFilePath
-
 def create_security_token_from_csr():
         #Creating and returning token, secret - Farook
         try:
@@ -178,6 +178,9 @@ def add_Static_Valueto_Xml():
     gen.SetRefIdAttr("","invoiceSignedData")
     gen.AddObjectRef("xadesSignedProperties","sha256","","","http://www.w3.org/2000/09/xmldsig#SignatureProperties")
     return gen,sbXml  
+
+# add_Static_Valueto_Xml()
+# sys.exit()
 def load_certificate(): 
     gen ,sbXml= add_Static_Valueto_Xml()
     certFromPfx = chilkat2.Cert()
@@ -202,12 +205,14 @@ def load_certificate():
         print("success 69")
     return sbXml
 # load_certificate()
-   
+# sys.exit()
 def create_File_SignedXML():
     sbXml= load_certificate()
     success = sbXml.WriteFile("signedXml.xml","utf-8",False)
     # print(sbXml.GetAsString())
     return sbXml
+# create_File_SignedXML()
+# sys.exit()
 def zatca_Verification():
     sbXml = create_File_SignedXML()
     verifier = chilkat2.XmlDSig()
@@ -227,7 +232,9 @@ def zatca_Verification():
         verifyIdx = verifyIdx + 1
 
     print("All signatures were successfully verified.")
+    
 # zatca_Verification()
+# sys.exit()
 def qrcode_Creation():
     sellerName = "Firoz Ashraf"
     vatNumber = "1234567891"
@@ -240,28 +247,41 @@ def qrcode_Creation():
     bdTlv.AppendByte(tag)
     bdTlv.AppendCountedString(1,False,sellerName,charset)
     tag = tag + 1
+    
+    length = len( bdTlv.GetEncoded("base64") )
+    print("Length of the tag1:", length)
     # This is tag 2
     bdTlv.AppendByte(tag)
     bdTlv.AppendCountedString(1,False,vatNumber,charset)
     tag = tag + 1
+    length = len( bdTlv.GetEncoded("base64") )
+    print("Length of the tag2:", length)
     # This is tag 3
     bdTlv.AppendByte(tag)
     bdTlv.AppendCountedString(1,False,timeStamp,charset)
     tag = tag + 1
+    length = len( bdTlv.GetEncoded("base64") )
+    print("Length of the tag3:", length)
     # This is tag 4
     bdTlv.AppendByte(tag)
     bdTlv.AppendCountedString(1,False,invoiceTotal,charset)
     tag = tag + 1
+    length = len( bdTlv.GetEncoded("base64") )
+    print("Length of the tag4:", length)
     # This is tag 5
     bdTlv.AppendByte(tag)
     bdTlv.AppendCountedString(1,False,vatTotal,charset)
-
+    length = len( bdTlv.GetEncoded("base64") )
+    print("Length of the tag5:", length)
     sbDigestValue, xmlSigned, signedXmlFilePath = get_signed_xml_invoice_for_clearance()
     print("success upto 153")
+  
     tag = 6
     bdTlv.AppendByte(tag)
     bdTlv.AppendByte(sbDigestValue.Length)
     bdTlv.AppendSb(sbDigestValue,"utf-8")
+    length = len( bdTlv.GetEncoded("base64") )
+    print("Length of the tag6:", length)
     sbSignatureValue = chilkat2.StringBuilder()
     success = xmlSigned.GetChildContentSb("ext:UBLExtensions|ext:UBLExtension|ext:ExtensionContent|sig:UBLDocumentSignatures|sac:SignatureInformation|ds:Signature|ds:SignatureValue",sbSignatureValue)
     if (success == False):
@@ -272,42 +292,50 @@ def qrcode_Creation():
     bdTlv.AppendByte(tag)
     bdTlv.AppendByte(sbSignatureValue.Length)
     bdTlv.AppendSb(sbSignatureValue,"utf-8")
-    x509Certificate = xmlSigned.GetChildContent("ext:UBLExtensions|ext:UBLExtension|ext:ExtensionContent|sig:UBLDocumentSignatures|sac:SignatureInformation|ds:Signature|ds:KeyInfo|ds:X509Data|ds:X509Certificate")
-    if (xmlSigned.LastMethodSuccess == False):
-        print("Failed to get X509Certificate from the signed XML.")
-        sys.exit()
+    length = len( bdTlv.GetEncoded("base64") )
+    print("Length of the tag7:", length)
+    # x509Certificate = xmlSigned.GetChildContent("ext:UBLExtensions|ext:UBLExtension|ext:ExtensionContent|sig:UBLDocumentSignatures|sac:SignatureInformation|ds:Signature|ds:KeyInfo|ds:X509Data|ds:X509Certificate")
+    # if (xmlSigned.LastMethodSuccess == False):
+    #     print("Failed to get X509Certificate from the signed XML.")
+    #     sys.exit()
+    # print()
 
-    cert = chilkat2.Cert()
+    # cert = chilkat2.Cert()
 
-    success = cert.SetFromEncoded(x509Certificate)
-    if (success == False):
-        print("Failed to load signing certificate from base64.")
-        sys.exit()
-    bdPubKey = chilkat2.BinData()
-    success = cert.GetPubKeyDer(True,bdPubKey)
-    if (success == False):
-        print("Failed to get certificate's public key.")
-        sys.exit()
-    tag = 8
-    bdTlv.AppendByte(tag)
-    bdTlv.AppendByte(bdPubKey.NumBytes)
-    bdTlv.AppendBd(bdPubKey)
-    # print("Certificate public key:")
-    # print(bdPubKey.GetEncoded("base64"))
-    bdCertSig = chilkat2.BinData()
-    success = cert.GetSignature(bdCertSig)
-    if (success == False):
-        print("Failed to get certificate's signature.")
-        sys.exit()
-    # else:
-    #     print("success 168")
-    tag = 9
-    bdTlv.AppendByte(tag)
-    bdTlv.AppendByte(bdCertSig.NumBytes)
-    bdTlv.AppendBd(bdCertSig)
-    # print("Certificate signature:")
-    # print(bdCertSig.GetEncoded("hex"))
-    
+    # success = cert.SetFromEncoded(x509Certificate)
+    # if (success == False):
+    #     print("Failed to load signing certificate from base64.")
+    #     sys.exit()
+    # bdPubKey = chilkat2.BinData()
+    # success = cert.GetPubKeyDer(True,bdPubKey)
+    # if (success == False):
+    #     print("Failed to get certificate's public key.")
+    #     sys.exit()
+    # tag = 8
+    # bdTlv.AppendByte(tag)
+    # bdTlv.AppendByte(bdPubKey.NumBytes)
+    # bdTlv.AppendBd(bdPubKey)
+    # length = len( bdTlv.GetEncoded("base64") )
+    # print("Length of the tag8:", length)
+    # # print("Certificate public key:")
+    # # print(bdPubKey.GetEncoded("base64"))
+
+    # bdCertSig = chilkat2.BinData()
+    # success = cert.GetSignature(bdCertSig)
+    # if (success == False):
+    #     print("Failed to get certificate's signature.")
+    #     sys.exit()
+    # # else:
+    # #     print("success 168")
+    # tag = 9
+    # bdTlv.AppendByte(tag)
+    # bdTlv.AppendByte(bdCertSig.NumBytes)
+    # bdTlv.AppendBd(bdCertSig)
+    # length = len( bdTlv.GetEncoded("base64") )
+    # print("Length of the tag9:", length)
+    # # print("Certificate signature:")
+    # # print(bdCertSig.GetEncoded("hex"))
+    # # print(bdTlv.GetEncoded("base64"))
     qr_base64 = bdTlv.GetEncoded("base64")
     # print("QR: " + qr_base64)
 
@@ -328,15 +356,21 @@ def qrcode_Creation():
     return bdTlv
 # qrcode_Creation()
 # sys.exit()
+def hardcode_qrcode():
+    qrcode=" AQxGaXJveiBBc2hyYWYCCjEyMzQ1Njc4OTEDEzIwMjEtMTEtMTcgMDg6MzA6MDAEBjEwMC4wMAUFMTUuMDAGLHhHNWhNM2VtL0E2ajJPNUg0Q2g1TkdFL01NSVhycEdkWFNpYVo0NkRmYjg9B/9qZ0UzSXpxaElSczJ4TnAyWFBxZCtsQzVXNEpXQWhka056NVhkOGtKQUhCSFUzWGV2N1FQQWRseXM5UEtGRlN1c0JPWkpRSmJyTEN5MWYwMHNncWFSdkxndFk1dW1XN0xXMnJiYnBLNzhobnptY24xOGh6c2RyKzdjeFMrRGR5cmk1aSt1V2I5QWpNVmhVR3NWa3FOcjJaMXVQNzJzbzJmbUlXOGlBRFJ0cmtPWmFYMkdJemE4bDYwaEN3UFpTdFpnNFNmVkVWbk5VcXlXbUZWZi9YdkViYXdnQVg0SEtqeGIwNE5JSXhpUFhZUVZ0ZjBwakhWQUVidUwvanp2KzFRWkZOKzRNNG92RjFvWnpaUDVLNEdZZExmRG91bFhCbFdnbFFYbFJyLzBIY2JieEhRZWgzQnJUbHpJeVdSRVpQOHlsd0Y1WWVRQkxxbGRHc2RKMkN1dFE9PQj/MIIBCgKCAQEAvUI4k3a7/XoBCbm8hul3r8otO4Nr9B4f8Lz71J03B12RUTUaLoDVVcbKIBZNLFcgFS4CXhGmwF3BfDXEwz4O/LXI9LXfcSGrENp0VhHmQaobNxnNKiL+5CFy/zo1EymH69t2dTFxpY92cgaatQLJoaAmWyrJGzKa0irxu1XXEfQlkko65NNzmLg6xvIhJNDFE7l1ddPu7sfQFi2Xp/CTcUy6x0sQFVyQ6MBdJVVuY1KzRhg3EBN/a/DHDYULFShD8Me+VX95/d/2T8nGyWETjGIEpId6zk0xbRNax5XpXsBDxf7qlIchagMCWRs2H8Pt6zCZj5Y79iR1LLcj2zeVbQIDAQABCf898I0jHdhvolPaBvvg+DRNnm9xPu7iCNwXFMqxK/vyZDP8VcNLx8wOdwYlDVNBgnclgpwUU3WoqIKyF44U7fWN1MRMl9fGQa7AOtKxlkJ8vWdDPMzY03J1rrfWpA0FYShLzKoAmCAETOnv6Xk3qqvmh4t1VAWQsWoBG8rWhnrGDc6CBl3O36CLJO6qJpXvPoUOQ/Jz0QeRl6YMDOQIi0npnKfOlU5snSHshz2H9uxTHvQZGs9d1xfijLgqQqr/twZQKvs0H8+T4x1q5V3VnvdrqIz/RT6YQp0C46baCbrveYaspy/Y/3HRouU3IfHEA1Pc/Da3n5/gTJcc/PSjXdOU"
+    print("success qr code creation")
+    return qrcode
+
 def add_QRcodeto_Xml():
     bdTlv = qrcode_Creation()
+    # qr=hardcode_qrcode()
     signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXml.xml"    # doubt to pass signedxml file path
     xmlQR = chilkat2.Xml()
     xmlQR.Tag = "cac:AdditionalDocumentReference"
     xmlQR.UpdateChildContent("cbc:ID","QR")
     xmlQR.UpdateAttrAt("cac:Attachment|cbc:EmbeddedDocumentBinaryObject",True,"mimeCode","text/plain")
     # xmlQR.UpdateChildContent("cac:Attachment|cbc:EmbeddedDocumentBinaryObject",bdTlv.GetEncoded("base64"))
-    xmlQR.UpdateChildContent("cac:Attachment|cbc:EmbeddedDocumentBinaryObject","Testing my QR Code 333")
+    # xmlQR.UpdateChildContent("cac:Attachment|cbc:EmbeddedDocumentBinaryObject","Testing my QR Code 333")
     sbSignedXml = chilkat2.StringBuilder()
     success = sbSignedXml.LoadFile(signedXmlFilePath,"utf-8")
     if (success == False):
@@ -364,7 +398,7 @@ def add_QRcodeto_Xml():
     sample_string = '''GsiuvGjvchjbFhibcDhjv1886G'''
     # print(sbReplaceStr.GetAsString())
     success = sbSignedXml.ReplaceFirst(sample_string,bdTlv.GetEncoded("base64"))
-
+    # success = sbSignedXml.ReplaceFirst(sample_string,qr)
     if success == False:
         print("Failed to replace <cac:Signature> with QR code in the signed XML.")
         sys.exit()
@@ -378,9 +412,10 @@ def add_QRcodeto_Xml():
         sys.exit()
 
     success = sbSignedXml.WriteFile("signedXML_withQR.xml","utf-8",False)
+    print("suceess add qr code")
 # add_QRcodeto_Xml()
+# sys.exit()
     return sbSignedXml
-
 def verify_SignXML_withQR():
     sbSignedXml = add_QRcodeto_Xml()
     verifier = chilkat2.XmlDSig()
@@ -404,8 +439,10 @@ def verify_SignXML_withQR():
         verifyIdx = verifyIdx + 1
     print("All signatures were successfully verified.")
 # verify_SignXML_withQR()
+# sys.exit()
 def signedXml_Withtoken():
     otp = "123345"
+    # signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXML_withQR.xml"
     signedXmlFilePath = "/opt/oxy/frappe-bench/sites/signedXml.xml"    # doubt to pass signedxml file path
     token,secret = create_security_token_from_csr()
     print("success line upto 403")
@@ -422,6 +459,8 @@ def signedXml_Withtoken():
     else:
          print("success 407")
     return signedXml
+# signedXml_Withtoken()
+# sys.exit()
 def get_InvoiceHash():
     signedXml = signedXml_Withtoken()
     invoiceHash = signedXml.GetChildContent("ext:UBLExtensions|ext:UBLExtension|ext:ExtensionContent|sig:UBLDocumentSignatures|sac:SignatureInformation|ds:Signature|ds:SignedInfo|ds:Reference[0]|ds:DigestValue")
@@ -429,11 +468,11 @@ def get_InvoiceHash():
     # return 
     return invoiceHash
 # get_InvoiceHash()
-
+# sys.exit()
 def  get_UUID():
     signedXml = signedXml_Withtoken()
     cbc_UUID = signedXml.GetChildContent("cbc:UUID")
-    print(cbc_UUID)
+    # print(cbc_UUID)
     return cbc_UUID
 # get_UUID()
 
