62f668c8d644f6a75061143467efa86a53eaeece
HusnaMa <husna@htsqatar.com>
2023-10-25 05:43:36 +0000

    BUTTON call

--- saudi_phase2_api/public/js/our_sales_invoice.js
+++ saudi_phase2_api/public/js/our_sales_invoice.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Sales Invoice", {
     refresh: function(frm) {
         frm.add_custom_button(__("click"), function() {
             frm.call({
-                method: "saudi_phase2_api.saudi_phase2_api.myzatca_test.clickbutton",
+                method: "clickbutton",
                 args: {
 
                 },
